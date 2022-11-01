d2c868f65001fef90fe52add927cc493b69121ee
tarasenko-d <dartaras21@gmail.com>
2022-11-01 21:54:59 +0300

    +2 classes

---
+++ src/main/java/mvc/config/CarProperty.java
@@ -0,0 +1,16 @@
+package mvc.config;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "cars")
+public class CarProperty {
+
+    private int maxCars;
+}
